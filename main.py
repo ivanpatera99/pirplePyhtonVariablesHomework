1873ffd9dd1d7cf61f80fc89df3283e17a1c8f72
Ivan Patera <ivanpatera@iMac.local>
2019-05-20 14:37:30 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#Shape of you metadata
+Name = "Shape of you" #Name of the song
+Artist = "Ed Sheeran" #Artist's name
+Album = "÷" #Album where the song appears
+Genre = "Pop" #Song's genre
+YearReleased = 2017 #Year of release
+DurationSeconds = 233 #Duration in seconds
+DurationMinutes = 2.88 #Duration in minutes
+RecordedYear = 2016 #Year of recording
+Studio = "Rokstone studios" #Studio where the song was recorded
+PlaceOfRecording = "London" #Place of recording
+
+print(Name)
+print(Artist)
+print(Album)
+print(Genre)
+print(YearReleased)
+print(DurationSeconds)
+print(DurationMinutes)
+print(RecordedYear)
+print(Studio)
+print(PlaceOfRecording)
